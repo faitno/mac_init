860548611eafa75bca05a89257e52566c543a1d1
Alexey Ch <faitnomor@gmail.com>
2019-11-08 10:39:22 +0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -60,6 +60,7 @@ brew cask install hex-fiend
 brew cask install cyberduck
 #brew cask install cyberduck@6.6.1 - don't respond @buy me!
 brew cask install gimp
+brew cask install xquartz
 brew cask install inkscape
 brew cask install calibre
 brew cask install iterm2
