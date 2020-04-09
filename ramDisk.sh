8e36148eb74e4e0bad505fb35d719378435a7ea2
Alexey Ch <faitnomor@gmail.com>
2020-04-09 11:57:54 +0500

    Update ramDisk.sh

--- ramDisk.sh
+++ ramDisk.sh
@@ -1,4 +1,5 @@
-ramfs_size_mb=1024
+#!/bin/bash
+ramfs_size_mb=1536
 ramfs_size_sectors=$((ramfs_size_mb*1024*1024/512))
 mount_point=/Users/${USER}/ramdisk
 ramdisk_device=$(hdid -nomount ram://${ramfs_size_sectors} | xargs)
@@ -8,6 +9,12 @@ umount -f "${mount_point}"
 newfs_hfs -v 'ramdisk' "${ramdisk_device}"
 mkdir -p "${mount_point}"
 mount -o noatime -t hfs "${ramdisk_device}" "${mount_point}"
+mdutil -i off "${mount_point}"
+#rm -rf "${mount_point}"/{,_.}{fseventsd,Spotlight-V*,Trashes}
+rm -rf "${mount_point}"/{,_.}{fseventsd,Spotlight-V*}
+mkdir "${mount_point}"/.fseventsd
+#touch "${mount_point}"/.fseventsd/no_log "${mount_point}"/.metadata_never_index "${mount_point}"/.Trashes
+touch "${mount_point}"/.fseventsd/no_log "${mount_point}"/.metadata_never_index
 
 /bin/mkdir -p "${USERRAMDISK}"/temp
 /bin/mkdir -p "${USERRAMDISK}"/wget
@@ -39,3 +46,15 @@ mount -o noatime -t hfs "${ramdisk_device}" "${mount_point}"
 /bin/rm -rf ~/Library/Caches/com.sequelpro.SequelPro
 /bin/mkdir -p "${USERRAMDISK}"/Library/Caches/com.sequelpro.SequelPro
 /bin/ln -s "${USERRAMDISK}"/Library/Caches/com.sequelpro.SequelPro ~/Library/Caches/com.sequelpro.SequelPro
+
+/bin/rm -rf ~/Library/"Application Support"/VSCodium/Cache
+/bin/mkdir -p "${USERRAMDISK}"/Library/"Application Support"/VSCodium/Cache
+/bin/ln -s "${USERRAMDISK}"/Library/"Application Support"/VSCodium/Cache ~/Library/"Application Support"/VSCodium/Cache
+
+/bin/rm -rf ~/Library/"Application Support"/VSCodium/CachedData
+/bin/mkdir -p "${USERRAMDISK}"/Library/"Application Support"/VSCodium/CachedData
+/bin/ln -s "${USERRAMDISK}"/Library/"Application Support"/VSCodium/CachedData ~/Library/"Application Support"/VSCodium/CachedData
+
+/bin/rm -rf ~/Library/"Application Support"/VSCodium/CachedExtensions
+/bin/mkdir -p "${USERRAMDISK}"/Library/"Application Support"/VSCodium/CachedExtensions
+/bin/ln -s "${USERRAMDISK}"/Library/"Application Support"/VSCodium/CachedExtensions ~/Library/"Application Support"/VSCodium/CachedExtensions
