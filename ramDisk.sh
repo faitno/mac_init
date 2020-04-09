f5405cd320699022484167cd25c2c48af0f259b8
Alexey Ch <faitnomor@gmail.com>
2020-04-09 11:51:28 +0500

    Update ramDisk.sh

--- ramDisk.sh
+++ ramDisk.sh
@@ -6,7 +6,47 @@ ramdisk_device=$(hdid -nomount ram://${ramfs_size_sectors} | xargs)
 USERRAMDISK="${mount_point}"
 
 umount -f "${mount_point}"
+newfs_hfs -v 'ramdisk' "${ramdisk_device}"ramfs_size_mb=1024
+ramfs_size_sectors=$((ramfs_size_mb*1024*1024/512))
+mount_point=/Users/${USER}/ramdisk
+ramdisk_device=$(hdid -nomount ram://${ramfs_size_sectors} | xargs)
+USERRAMDISK="${mount_point}"
+
+umount -f "${mount_point}"
 newfs_hfs -v 'ramdisk' "${ramdisk_device}"
+mkdir -p "${mount_point}"
+mount -o noatime -t hfs "${ramdisk_device}" "${mount_point}"
+
+/bin/mkdir -p "${USERRAMDISK}"/temp
+/bin/mkdir -p "${USERRAMDISK}"/wget
+
+/bin/rm -rf ~/Downloads
+/bin/mkdir -p "${USERRAMDISK}"/Downloads
+/bin/ln -s "${USERRAMDISK}"/Downloads ~/Downloads
+
+/bin/rm -rf ~/Library/Caches/Google
+/bin/mkdir -p "${USERRAMDISK}"/Library/Caches/Google
+/bin/ln -s "${USERRAMDISK}"/Library/Caches/Google ~/Library/Caches/Google
+
+/bin/rm -rf ~/Library/Caches/Firefox
+/bin/mkdir -p "${USERRAMDISK}"/Library/Caches/Firefox
+/bin/ln -s "${USERRAMDISK}"/Library/Caches/Firefox ~/Library/Caches/Firefox
+
+/bin/rm -rf ~/Library/Caches/com.apple.Safari
+/bin/mkdir -p "${USERRAMDISK}"/Library/Caches/com.apple.Safari
+/bin/ln -s "${USERRAMDISK}"/Library/Caches/com.apple.Safari ~/Library/Caches/com.apple.Safari
+
+/bin/rm -rf ~/Library/Caches/com.apple.Safari.SafeBrowsing
+/bin/mkdir -p "${USERRAMDISK}"/Library/Caches/com.apple.Safari.SafeBrowsing
+/bin/ln -s "${USERRAMDISK}"/Library/Caches/com.apple.Safari.SafeBrowsing ~/Library/Caches/com.apple.Safari.SafeBrowsing
+
+/bin/rm -rf ~/Library/Caches/com.apple.iTunes
+/bin/mkdir -p "${USERRAMDISK}"/Library/Caches/com.apple.iTunes
+/bin/ln -s "${USERRAMDISK}"/Library/Caches/com.apple.iTunes ~/Library/Caches/com.apple.iTunes
+
+/bin/rm -rf ~/Library/Caches/com.sequelpro.SequelPro
+/bin/mkdir -p "${USERRAMDISK}"/Library/Caches/com.sequelpro.SequelPro
+/bin/ln -s "${USERRAMDISK}"/Library/Caches/com.sequelpro.SequelPro ~/Library/Caches/com.sequelpro.SequelPro
 mkdir -p "${mount_point}"
 mount -o noatime -t hfs "${ramdisk_device}" "${mount_point}"
 mdutil -i off "${mount_point}"
