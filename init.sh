a833fca74bb386b327811e337f7c54010b57ef34
Alexey Ch <faitnomor@gmail.com>
2021-06-23 16:31:17 +0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -135,6 +135,8 @@ brew install https://raw.githubusercontent.com/kadwanev/bigboybrew/master/Librar
 #brew install e2fsprogs #hdd repair/scan #diskutil list #/usr/local/Cellar/e2fsprogs/1.44.3/sbin/badblocks -v /dev/diskX
 #packet_write_wait: Connection to *.*.*.* port 22: Broken pipe
 brew install proxychains-ng -s #Where the -s tells brew to compile from source.
+#proxychains4 node app.js --name=xParserDaemon
+#/usr/local/etc/proxychains.conf
 
 
 #sudo nano /etc/ssh/ssh_config
