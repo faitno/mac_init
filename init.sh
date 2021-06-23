a87d9a2a47b3feefd4d3abaca99e945e9d54b90d
Alexey Ch <faitnomor@gmail.com>
2021-06-23 18:16:05 +0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -123,6 +123,7 @@ brew cask install plistedit-pro
 brew cask install amorphousdiskmark #ssd perfomance check
 brew cask install coconutbattery
 brew install --cask adium
+brew install --cask controlplane
 brew install --cask openmtp #android file transfer
 #https://www.fosshub.com/Audacity.html/audacity-macos-2.3.1.dmg
 ###!!!!brew cask install clean-me - break system!
