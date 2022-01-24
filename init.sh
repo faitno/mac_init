5fd25bdde0744a66818d18f9b32cda19d0b98a65
Alexey Ch <faitnomor@gmail.com>
2022-01-24 05:58:07 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -155,7 +155,7 @@ brew install mosh #for connect over LTE with change IP without reconnect
 brew install proxychains-ng -s #Where the -s tells brew to compile from source.
 #proxychains4 node app.js --name=xParserDaemon
 #/usr/local/etc/proxychains.conf
-
+brew install --cask motrix #perfect download manager
 
 #sudo nano /etc/ssh/ssh_config
 #Host *
