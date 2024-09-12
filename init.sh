ad97c293a6c06c52c2884bdab52abd213a759ca8
Alexey Ch <faitnomor@gmail.com>
2024-09-12 14:49:39 +0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -205,6 +205,8 @@ HOMEBREW_NO_AUTO_UPDATE=1 brew install --cask --no-quarantine controlplane
 HOMEBREW_NO_AUTO_UPDATE=1 brew install --cask --no-quarantine openmtp #android file transfer
 HOMEBREW_NO_AUTO_UPDATE=1 brew install --cask --no-quarantine librecad #autocad free alternative
 HOMEBREW_NO_AUTO_UPDATE=1 brew install --cask --no-quarantine switchresx #custom herz on monitor
+HOMEBREW_NO_AUTO_UPDATE=1 brew install spoofdpi #Simple and fast anti-censorship tool written in Go youtube rutracker unblock
+#.spoofdpi/bin/spoofdpi -system-proxy=false
 #https://www.fosshub.com/Audacity.html/audacity-macos-2.3.1.dmg
 ###!!!!HOMEBREW_NO_AUTO_UPDATE=1 brew install --cask --no-quarantine clean-me - break system!
 #brew install wine  $El capitan
